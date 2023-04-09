bf479fd98f5746735b270d4955075d66d641cca6
clementigbinoba <108894960+clementigbinoba@users.noreply.github.com>
2023-04-09 12:05:49 -0700

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,1 @@
-#!/usr/bin/python3
 
-"""
-This module contain the Rectangle class.
-"""
-
-class Rectangle:
-	"""
-	A class that represents a rectangle.
-	Currently an empty class with no properties or methods.
-	"""
-	
