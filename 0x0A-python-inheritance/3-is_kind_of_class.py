67fabcc48d8170f1e68161cbd1693492e8f74941
clementigbinoba <108894960+clementigbinoba@users.noreply.github.com>
2023-04-11 13:44:01 -0700

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
-Contains the is_kind_of_class function
+has class function
 """
 
-
 def is_kind_of_class(obj, a_class):
-    """True if obj is an instance or inherited from a_class, else False"""
-    return (isinstance(obj, a_class))
+    """True if obj is inherited from a_class, else False"""
+    return type(obj) is a_class or issubclass(type(obj), a_class)
