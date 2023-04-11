e1b81b59cd8e549f16885c38fd3882a7a4513ee0
clementigbinoba <108894960+clementigbinoba@users.noreply.github.com>
2023-04-11 13:10:22 -0700

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,10 +3,13 @@
 contains the MyList class
 """
 
-
 class MyList(list):
+    def __init__(self, *args, **kwargs):
+        super(MyList, self).__init__(*args, **kwargs)
+        
     def get_first(self):
         return self[0] if len(self) > 0 else None
+
 
 
 
