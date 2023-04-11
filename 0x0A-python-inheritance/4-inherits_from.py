1504472f31544989e94ecd903977bd57b2f09eba
clementigbinoba <108894960+clementigbinoba@users.noreply.github.com>
2023-04-11 13:54:38 -0700

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,5 +4,5 @@ have inherits_of function
 """
 
 def inherits_from(obj, a_class):
-    """Returns True if `obj` is a subclass of `a_class`, otherwise False."""
+    """Returns True if `obj` is subclass of `a_class`, else False."""
     return issubclass(type(obj), a_class) and type(obj) is not a_class
