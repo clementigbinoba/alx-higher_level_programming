8c733058a55e244d6ba94c9f2918cdabb57dd3ce
clementigbinoba <108894960+clementigbinoba@users.noreply.github.com>
2023-04-11 13:08:41 -0700

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,13 +5,8 @@ contains the MyList class
 
 
 class MyList(list):
-    def __init__(self):
-        list.__init__(self)
-        
     def get_first(self):
-        if len(self) > 0:
-            return self[0]
-        else:
-            return None
+        return self[0] if len(self) > 0 else None
+
 
 
